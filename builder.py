74b3fd1bdd0d8e439ea832687e40fb1f374a97e5
Axel Souchet <1476421+0vercl0k@users.noreply.github.com>
2020-07-26 14:34:56 -0700

    Separate tests (#4)
    This PR separates the testing into a CMake target to allow #3 to work properly with various flavor of Python for the extension.
    On top of that, it moves off the Linux CI to Github actions (Windows will follow).

--- builder.py
+++ builder.py
@@ -8,12 +8,11 @@ import itertools
 import platform
 import argparse
 
-os_prefix = '' if 'Windows' in platform.platform(terse = 1) else 'lin'
-is_windows = os_prefix == ''
+os_prefix = 'win' if 'Windows' in platform.platform(terse = 1) else 'lin'
+is_windows = os_prefix == 'win'
 is_linux = not is_windows
 is_linux64 = is_linux and platform.architecture()[0] == '64bit'
 
-testdatas_url = 'https://github.com/0vercl0k/kdmp-parser/releases/download/v0.1/testdatas.zip'
 vsdevprompt = r'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat'
 
 def source_bat(bat_file, arch):
@@ -36,7 +35,7 @@ def source_bat(bat_file, arch):
 
     return result
 
-def build(arch, configuration):
+def build(arch, configuration, tests_on):
     # Grab the environment needed for the appropriate arch on Windows.
     env = source_bat(vsdevprompt, arch) if is_windows else os.environ
     dir_name = f'{os_prefix}{arch}-{configuration}'
@@ -62,6 +61,7 @@ def build(arch, configuration):
         'cmake',
         f'-DCMAKE_RUNTIME_OUTPUT_DIRECTORY={output_dir}',
         f'-DCMAKE_BUILD_TYPE={configuration}',
+        f'-DBUILD_TESTS={tests_on}'
     )
     cmake_config += extra_opts
     cmake_config += (
@@ -81,17 +81,6 @@ def build(arch, configuration):
 
     return ret
 
-def test(arch, configuration, dmp_path):
-    dir_name = f'{os_prefix}{arch}-{configuration}'
-    bin_dir = os.path.join('bin', dir_name)
-    cmd = (
-        os.path.join(bin_dir, 'testapp'),
-        dmp_path
-    )
-
-    print('Launching "{0}"..'.format(' '.join(cmd)))
-    return subprocess.call(cmd)
-
 def main():
     parser = argparse.ArgumentParser('Build and run test')
     parser.add_argument('--run-tests', action = 'store_true', default = False)
@@ -100,51 +89,21 @@ def main():
     args = parser.parse_args()
 
     if args.configuration is None:
-        args.configuration = ['Debug', 'RelWithDebInfo']
+        args.configuration = ('Debug', 'RelWithDebInfo')
 
     if args.arch is None:
-        args.arch = ['x64', 'x86']
+        args.arch = ('x64', 'x86')
 
     matrix = tuple(itertools.product(
         args.arch,
         args.configuration
     ))
 
+    tests_on = 'ON' if args.run_tests else 'OFF'
     for arch, configuration in matrix:
-        if build(arch, configuration) != 0:
+        if build(arch, configuration, tests_on) != 0:
             print(f'{arch}/{configuration} build failed, bailing.')
             return 1
-
-    if not args.run_tests:
-        return 0
-
-    # Download the test datas off github.
-    print(f'Downloading {testdatas_url}..')
-    archive_path, _ = urllib.request.urlretrieve(testdatas_url)
-    print(f'Successfully downloaded the test datas in {archive_path}, extracting..')
-
-    # Unzip its content in the same temp directory.
-    archive_dir, _ = os.path.split(archive_path)
-    zipfile.ZipFile(archive_path).extractall(archive_dir)
-
-    # Once we have extracted the archive content, we can delete it.
-    os.remove(archive_path)
-
-    # Build full path for both the full / bitmap dumps.
-    full = os.path.join(archive_dir, 'full.dmp')
-    bmp = os.path.join(archive_dir, 'bmp.dmp')
-    dmp_paths = (full, bmp)
-
-    # Now iterate through all the configurations and run every flavor of test.exe against
-    # both dumps.
-    for dmp_path in dmp_paths:
-        for arch, configuration in matrix:
-            if test(arch, configuration, dmp_path) != 0:
-                print(f'{arch}/{configuration}/{dmp_path} test failed, bailing.')
-                return 1
-
-        # We've run this dump against all the flavor of the test application so we can delete it now.
-        os.remove(dmp_path)
 
     print('All good!')
     return 0
