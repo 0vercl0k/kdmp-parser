77c75f7a40536fa0afb8435db33b616c902eb867
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2020-12-27 09:50:14 -0800

    override

--- builder.py
+++ builder.py
@@ -57,6 +57,14 @@ def build(arch, configuration, tests_on):
             f'-DCMAKE_C_FLAGS=-m32'
         )
 
+    # Allow the user to override the Python version if Python_ROOT_DIR is found
+    # in the environment. This is particularly useful for the CI.
+    py_root = os.getenv('Python_ROOT_DIR')
+    if py_root is not None:
+        extra_opts = extra_opts + (
+            f'-DPython_ROOT_DIR={py_root}'
+        )
+
     cmake_config = (
         'cmake',
         f'-DCMAKE_RUNTIME_OUTPUT_DIRECTORY={output_dir}',
