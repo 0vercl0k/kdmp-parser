fb9b417bf40fe4c4304558f227c9d267e7440528
crazy hugsy <hugsy@users.noreply.github.com>
2024-07-13 19:26:53 -0700

    Add ARM64 support (#33)
    Co-authored-by: 0vercl0k <1476421+0vercl0k@users.noreply.github.com>

--- src/lib/platform.h
+++ src/lib/platform.h
@@ -5,6 +5,8 @@
 #define ARCH_X86
 #elif defined(__amd64__) || defined(_M_X64)
 #define ARCH_X64
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#define ARCH_ARM64
 #else
 #error Architecture not supported.
 #endif
@@ -14,10 +16,12 @@
 #define SYSTEM_PLATFORM "Windows"
 
 #include <windows.h>
-#if defined ARCH_X86
+#if defined(ARCH_X86)
 #define WINDOWS_X86
-#elif defined ARCH_X64
+#elif defined(ARCH_X64)
 #define WINDOWS_X64
+#elif defined(ARCH_ARM64)
+#define WINDOWS_ARM64
 #endif
 
 #elif defined(linux) || defined(__linux) || defined(__FreeBSD__) ||            \
@@ -27,21 +31,24 @@
 #if defined(linux) || defined(__linux)
 #define SYSTEM_PLATFORM "Linux"
 
-#if defined ARCH_X86
+#if defined(ARCH_X86)
 #define LINUX_X86
-#elif defined ARCH_X64
+#elif defined(ARCH_X64)
 #define LINUX_X64
+#elif defined(ARCH_ARM64)
+#define LINUX_ARM64
 #endif
 
 #elif defined(__APPLE__)
 
 #define SYSTEM_PLATFORM "OSX"
-#if defined ARCH_X86
+#if defined(ARCH_X86)
 #define OSX_X86
-#elif defined ARCH_X64
+#elif defined(ARCH_X64)
 #define OSX_X64
+#elif defined(ARCH_ARM64)
+#define OSX_ARM64
 #endif
-
 #else
 #error An error occured
 #endif
